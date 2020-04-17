f2414fed4f20af2a386616c594e1a92431e02990
Jinho <wjswlsgh94@naver.com>
2020-04-17 20:12:55 +0900

    interface

--- src/Project.java
+++ src/Project.java
@@ -6,56 +6,105 @@ import javax.swing.border.LineBorder;
 
 public class Project extends JFrame{
 
+
+
     public void main(){
+
+        //프레임 설정
         this.setSize(500,500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel northPanel = new JPanel(new GridLayout(0,3));
-        northPanel.add(new MenuPanel("imagePath"));
-        northPanel.add(new MenuPanel("imagePath"));
-        northPanel.add(new MenuPanel("imagePath"));
+        JPanel NorthPanel = new JPanel(new BorderLayout());
+        JPanel CenterPanel = new JPanel(new GridLayout(0,3));
+        JPanel SouthPanel = new JPanel(new BorderLayout());
 
-        this.getContentPane().setLayout(new BorderLayout());
-        this.getContentPane().add(northPanel,BorderLayout.NORTH);
+// 상단부
+
+        JLabel Title = new JLabel("Vending Machine");
+        Title.setFont(Title.getFont().deriveFont(20.0f));
+        Title.setHorizontalAlignment(SwingConstants.CENTER);
+        Title.setBounds(12, 10, 460, 29);
+        this.getContentPane().add(Title);
+
+
+
+        CenterPanel.add(new MenuPanel("imagePath"));
+        CenterPanel.add(new MenuPanel("imagePath"));
+        CenterPanel.add(new MenuPanel("imagePath"));
+
+        SouthPanel.add(new ListPanel(),BorderLayout.CENTER);
+        SouthPanel.add(new InputPanel(),BorderLayout.SOUTH);
+
+        NorthPanel.setPreferredSize(new Dimension(50,50));
+        CenterPanel.setPreferredSize(new Dimension(250,250));
+        SouthPanel.setPreferredSize(new Dimension(150,150));
+
+
+
+        this.getContentPane().add(NorthPanel,BorderLayout.NORTH);             // 상단부
+        this.getContentPane().add(CenterPanel,BorderLayout.CENTER);
+        this.getContentPane().add(SouthPanel,BorderLayout.SOUTH);          // 하단부분
         this.setVisible(true);
     }
 
-    ///상단부
+
+
+    ///중단부
     public class MenuPanel extends JPanel {
-        JButton button1= new JButton("+");
-        JButton button2= new JButton("-");
+        JButton button1 = new JButton("buy");
+        JButton button2 = new JButton("reduce");
 
         public MenuPanel(String imagePath) {
             this.setLayout(new BorderLayout());
-            JLabel label= new JLabel(imagePath);
-            this.add(label,BorderLayout.NORTH);
+            JLabel label = new JLabel(imagePath);
+            this.add(label, BorderLayout.CENTER);
             this.setBorder(new LineBorder(Color.BLACK));
 
             JPanel buttonPanel = new JPanel();
             buttonPanel.add(button2);
             buttonPanel.add(button1);
-            this.add(buttonPanel,BorderLayout.SOUTH);
+            this.add(buttonPanel, BorderLayout.SOUTH);
         }
-
-    //중간
-        public class statusPanel extends JPanel {
-    JButton button3 = new JButton("입력");
-
-    public statusPanel(){
-        this.setLayout(new BorderLayout());
-        JLabel current = new JLabel();
-        this.add(current,BorderLayout.CENTER);
     }
-    }
-
-
-
-
 
 
     //하단
+public class ListPanel extends JPanel{
+        JTextField List = new JTextField("contents");
+        JTextField Total = new JTextField("total fee");
+        JPanel BottomLabel = new JPanel(new BorderLayout());
 
+        public ListPanel() {
+            this.setLayout(new BorderLayout());
+            this.add(List,BorderLayout.CENTER);
+           BottomLabel.add(Total,BorderLayout.EAST);
+            this.add(BottomLabel,BorderLayout.SOUTH);
+
+        }
     }
+
+
+    public class InputPanel extends  JPanel {
+        JButton Purchase = new JButton("buy");
+        JTextField Price = new JTextField("blabla");
+
+        public InputPanel(){
+            this.setLayout(new BorderLayout());
+            this.add(Purchase,BorderLayout.EAST);
+            this.add(Price,BorderLayout.CENTER);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
